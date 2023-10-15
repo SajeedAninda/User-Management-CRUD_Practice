300dc5579372ae59794a692c899ee3ba827d3bf8
SajeedAninda <sajaninda@gmail.com>
2023-10-15 16:28:49 +0600

    Added Navbar

---
+++ user-management-client/src/Homepage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Homepage = () => {
+    return (
+        <div className='w-full h-full'> 
+            
+        </div>
+    );
+};
+
+export default Homepage;
---
+++ user-management-client/src/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div className='bg-emerald-400 text-center py-5'>
+            <h1 className='text-gray-800 text-3xl font-bold'>User Management System</h1>
+        </div>
+    );
+};
+
+export default Navbar;
